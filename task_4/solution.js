13c44d610bda38de443e303de0c76c658840c8d2
Aleksandra Guseletova <alekseeva-aleksandra-2013@mail.ru>
2022-02-20 18:03:30 +0700

    task4 change product - done

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,11 +1,12 @@
 function parseProducts(json) {
     var data = JSON.parse(json);
-    return data.product;
+    return data.products;
 }
 
 function renderProductsCards(json) {
     clearProducts();
     let products = parseProducts(json);
+    
     let length = products.length;
     for (let i = 0; i < length; i++) {
         addProduct(products[i]);
